026b1e86cce88818aa77724bd2237f8cd4412050
andreiprepelita <andrei_prepelita@yahoo.com>
2022-03-23 23:09:30 +0200

    update PublicationGeneratorConfiguration

--- src/main/java/publications/PublicationGeneratorConfiguration.java
+++ src/main/java/publications/PublicationGeneratorConfiguration.java
@@ -3,14 +3,14 @@ package publications;
 import java.util.Properties;
 
 public class PublicationGeneratorConfiguration {
-    private final int subscriptionsToGenerate;
+    private final int publicationsToGenerate;
 
     public PublicationGeneratorConfiguration(Properties properties) {
-        this.subscriptionsToGenerate = Integer.parseInt(properties.getProperty("NO_OF_PUBLICATIONS"));
+        this.publicationsToGenerate = Integer.parseInt(properties.getProperty("NO_OF_PUBLICATIONS"));
     }
 
     public int getPublicationsToGenerate() {
-        return subscriptionsToGenerate;
+        return publicationsToGenerate;
     }
 
 }
